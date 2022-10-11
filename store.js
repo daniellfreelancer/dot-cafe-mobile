a73d0d5d8a56db34fef6bf04f27feb26d6bce307
AmuchasteguiMarcos317 <amuchasteguimarcos317@gmail.com>
2022-10-11 18:04:58 -0300

    fix signin

--- store.js
+++ store.js
@@ -2,6 +2,8 @@ import { configureStore }   from "@reduxjs/toolkit";
 import { setupListeners } from '@reduxjs/toolkit/query'
 import coffeeByKiloAPI from "./src/features/coffeeByKiloAPI";
 import cartReducer from './src/features/cartSlice'
+import usersAPI from './src/features/usersAPI'
+import userReducer from './src/features/usersSlice'
 
 const store = configureStore({
 
@@ -9,6 +11,7 @@ const store = configureStore({
     reducer: {
         cart: cartReducer,
         [coffeeByKiloAPI.reducerPath]: coffeeByKiloAPI.reducer,
+        [usersAPI.reducerPath] : usersAPI.reducer,
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(coffeeByKiloAPI.middleware ),
 
